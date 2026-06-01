92dd458e06d1bedd129d86386d7341596f5bed5d
Chris Rodriguez Medina <github@crod.org>
2026-06-01 19:16:25 -0400

    helm 0.2.0

--- Formula/helm.rb
+++ Formula/helm.rb
@@ -1,10 +1,10 @@
 class Helm < Formula
   desc "Share a tmux session between you and an AI agent, local or ssh-remote"
   homepage "https://github.com/crodorg/helm"
-  url "https://github.com/crodorg/helm/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "1bb7515b2e5cd287ae8fba24f53cfe199dfe76339f40b2433c609a7801feede7"
+  url "https://github.com/crodorg/helm/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "dc2cbb336a18898d72b2b91b0e8628bfa5457ec49b4dc1d8745e39870c8bb496"
   license "MIT"
-  version "0.1.1"
+  version "0.2.0"
 
   depends_on "rust" => :build
   depends_on "tmux"
